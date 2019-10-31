54d1a96ececb495d1bae9cfc12424347a8325ba5
alerubbio <rubio.alejandro7@knights.ucf.edu>
2019-10-30 20:33:01 -0400

    Comment JewelsAndStones

--- JewelsAndStones.java
+++ JewelsAndStones.java
@@ -1,3 +1,8 @@
+// You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  
+// Each character in S is a type of stone you have.  
+// You want to know how many of the stones you have are also jewels.
+
+// Solution: Store characters in J in HashSet. Search for characters of S in Set of J. O(n).
 class Solution {
     public int numJewelsInStones(String J, String S) {
         Set<Character> jSet = new HashSet<>();
