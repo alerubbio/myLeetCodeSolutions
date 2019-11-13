49501c9424f7bb9d57adbee1e1ca807bd17d3cf8
alerubbio <rubio.alejandro7@knights.ucf.edu>
2019-11-13 18:02:53 -0500

    Create UncommonWords

---
+++ Java/UncommonWords.java
@@ -0,0 +1,43 @@
+// Uncommon Words from Two Sentences
+
+// We are given two sentences A and B.  (A sentence is a string of space separated words.  
+// Each word consists only of lowercase letters.)
+// A word is uncommon if it appears exactly once in one of the sentences, 
+// and does not appear in the other sentence.
+// Return a list of all uncommon words. 
+// You may return the list in any order.
+
+
+// Solution: A word is basically uncommon if it is completely unique. Use HashMap to store
+// 
+class Solution {
+	public String[] uncommonFromSentences(String a, String b) 
+	{
+       HashMap<String, Integer> map = new HashMap<>();
+
+       for(String word: a.split(" "))
+       {
+       		if(map.get(word) == null)
+       			map.put(word, 0);
+       	map.put(word, map.get(word) + 1);
+       }
+
+       for(String word: b.split(" "))
+       {
+       		if(map.get(word) == null)
+       			map.put(word, 0);
+       	map.put(word, map.get(word) + 1);
+       }
+
+       ArrayList<String> list = new ArrayList<String>();
+
+       for(String word: map.keySet())
+       {
+       	if(map.get(word) == 1)
+       		list.add(word);
+       }
+
+       return list.toArray(new String[list.size()]);
+
+    }
+}
